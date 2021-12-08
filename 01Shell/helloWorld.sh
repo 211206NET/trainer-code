2cd39f5221a73f3c4e7fa9068d14a5467564c685
Juniper Song <songminseonk@gmail.com>
2021-12-08 14:22:43 -0500

    finished hello world

--- 01Shell/helloWorld.sh
+++ 01Shell/helloWorld.sh
@@ -16,14 +16,54 @@ echo "Say something"
 read word
 echo "You typed $word"
 
+
 echo "Do you want to say some more?"
 read answer
 
-if [ $answer = 'y' ]
-then 
-    echo "Say something"
-    read word
-    echo "You typed $word"
-else
-    echo "goodbye!"
-fi
+# creating this variable, and initially assign it to false
+exit=false
+
+while [ $exit = false ]
+do
+    # if answer variable is 'y' then execute the following block
+    if [ $answer = 'y' ]
+    then 
+        echo "Say something"
+        read word
+        echo "You typed $word"
+        echo "Continue speaking?"
+        read answer
+    # != compares two strings for inequality 
+    if [ $answer != 'y' ]
+    then
+        echo "goodbye!"
+        break
+    fi
+    # elif [ condition ]
+    # then
+    #     whatever you want to execute here
+    else
+        echo "goodbye!"
+        break
+    fi
+done
+
+#Loops: While, Until, For in
+
+# while loop executes as long as the condition evaluates to true
+# while [ condition ]
+# do
+# #commands here
+# done
+
+# until loop executes as long as the condition evaluates to false
+# until [ condition ]
+# do
+# #commands go here
+# done
+
+# for-in loop loops through a collection (ie array)
+# for elem in array
+# do
+# # Execute some stuff
+# done
