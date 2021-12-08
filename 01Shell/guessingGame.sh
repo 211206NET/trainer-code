862875faf3ef20266a0c2a459e715b51e6deb515
Juniper Song <songminseonk@gmail.com>
2021-12-08 15:17:30 -0500

    guessing game expected behavior

--- 01Shell/guessingGame.sh
+++ 01Shell/guessingGame.sh
@@ -3,6 +3,13 @@
 
 # Hint: Bash has built in function $RANDOM that generates a pseudo random number between 0 and 32767. use Modulo operator to restrict the bounds of the random number (ie, for a random number between 0 and 10, $RANDOM % 10)
 
+# Expected Behavior
+# First, generate a random number
+# ask user for a guess
+# tell them whether they were over or under and how far off they were
+# also tell them how many times they've tried so far
+
+
 randomNum=$(($RANDOM % 100))
 
 echo $randomNum
