bb6a0217f768e11c61624807ca6845d9fffef5eb
Juniper Song <songminseonk@gmail.com>
2021-12-09 12:32:31 -0500

    fizzbuzz cont

--- 01Shell/FizzBuzz/Program.cs
+++ 01Shell/FizzBuzz/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("FizzBuzz");
+// Console.WriteLine("Enter a positive integer");
+
+// //This is string variable
+// string input = Console.ReadLine();
+
+// //We are taking whatever user typed and printing it back out to the console
+// Console.WriteLine("You entered " + input);
+
+// //Next, we are parsing string input into number so we can do arithmetic on it
+// int number = int.Parse(input);
+
+// //While Loop (executes 0 or more)
+// while (number <= 0)
+// {
+//     Console.WriteLine("The number should be greater than 0");
+//     Console.WriteLine("Enter a positive number");
+//     input = Console.ReadLine();
+//     number = int.Parse(input);
+// }
+
+
+string input = "";
+int number = 0;
+
+//Do While Loop (executes 1 or more times)
+do
+{
+    Console.WriteLine("Enter a positive number");
+    input = Console.ReadLine();
+    number = int.Parse(input);
+
+    if(number <= 0)
+    {
+        Console.WriteLine("The number should be greater than 0");
+    }
+} while(number <= 0);
