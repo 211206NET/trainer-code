7452dc3179ff60f44da9f4a4c2048bf3a370ac42
Juniper Song <songminseonk@gmail.com>
2021-12-09 12:42:27 -0500

    fizzbuzz complete

--- 01Shell/FizzBuzz/Program.cs
+++ 01Shell/FizzBuzz/Program.cs
@@ -35,3 +35,32 @@ do
         Console.WriteLine("The number should be greater than 0");
     }
 } while(number <= 0);
+
+//Next I'm going to count up to the number and print them in console using while loop
+// int counter = 1;
+// while(counter <= number)
+// {
+//     Console.WriteLine(counter);
+//     counter++;
+// }
+
+//Another way to do this, is by using for loop
+for(int i = 1; i <= number; i++)
+{
+    if(i % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
