841206d03a493a2283e39e8cbbc15da66ebe82cf
Juniper Song <songminseonk@gmail.com>
2021-12-10 11:24:40 -0500

    todoItem

---
+++ 01Shell/ToDoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ToDo;
+
+Console.WriteLine("Welcome to ToDo List");
+Console.WriteLine("What would you like to do today?");
+Console.WriteLine("1. Create a new todo item");
+Console.WriteLine("2. View my list");
+Console.WriteLine("3. Complete an item");
+
+//instantiating an instance of our custom class
+ToDoItem item = new ToDoItem();
+//assigning values to the particular instance
+item.IsDone = false;
+item.Note = "Drink Coffee";
+
+//Here, we're using the class' methods
+Console.WriteLine(item.Print());
+
+item.CompleteItem();
+
+Console.WriteLine(item.Print());
