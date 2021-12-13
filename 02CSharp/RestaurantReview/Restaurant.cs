b46c26cb5a3b9aa5f2ba58feaa4985e4bcdf7b35
Juniper Song <songminseonk@gmail.com>
2021-12-13 16:00:55 -0500

    create reviews

--- 02CSharp/RestaurantReview/Restaurant.cs
+++ 02CSharp/RestaurantReview/Restaurant.cs
@@ -9,23 +9,37 @@ public class Restaurant {
     //public is the most visible, private is the least visible
     //By default, class member has private access modifier
     //Class, by default, are internal unless you explicitely say otherwise
+
+    public Restaurant()
+    {
+        this.Reviews = new List<Review>();
+    }
+    public Restaurant(string name)
+    {
+        this.Reviews = new List<Review>();
+        this.Name = name;
+    }
     public string Name { get; set; }
 
     // private string _name;
 
     // //our own custom getter and setter for the private backing field
-    // private string GetName() {
+    // public string GetName() {
     //     return this._name;
     // }
-
-    // private void SetName(string name)
+    // public void SetName(string name)
     // {
+    //     //input validation
+    //     if(name == "")
+    //     {
+    //         Console.WriteLine("Name Cannot Be Empty");
+    //     }
     //     this._name = name;
     // }
 
     // private string _city;
 
     public string City { get; set; }
-
     public string State { get; set; }
+    public List<Review> Reviews { get; }
 }
