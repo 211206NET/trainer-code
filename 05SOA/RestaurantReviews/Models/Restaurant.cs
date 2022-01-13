44d5521b8c38c5624683803ad6b02b32f9c08514
Juniper Song <songminseonk@gmail.com>
2022-01-13 12:42:35 -0500

    Model binding, dep injection, action methods, CR restaurant

--- 05SOA/RestaurantReviews/Models/Restaurant.cs
+++ 05SOA/RestaurantReviews/Models/Restaurant.cs
@@ -1,6 +1,7 @@
 using CustomExceptions;
 using System.Text.RegularExpressions;
 using System.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models;
 
@@ -21,7 +22,7 @@ public class Restaurant {
     public Restaurant(string name)
     {
         this.Reviews = new List<Review>();
-        this._name = name;
+        this.Name = name;
     }
 
     /// <summary>
@@ -38,31 +39,35 @@ public class Restaurant {
 
     public int Id { get; set; }
 
-    private string _name;
-    public string Name {
-        get => _name;
-        set
-        {
-            Regex pattern = new Regex("^[a-zA-Z0-9 !?']+$");
-            if(string.IsNullOrWhiteSpace(value))
-            {
-                //we're checking whether this string is null or whitespace
-                throw new InputInvalidException("Name can't be empty");
-            }
-            //even if the string is not null or white space,
-            //we can still check for the name's validity by using RegEx (Regular Expression)
-            //Regular Expression is a way to pattern match a string for a certain condition
-            //it has a confusing syntax, so I recommend looking up language specific RegEx reference page to build one
-            else if(!pattern.IsMatch(value))
-            {
-                throw new InputInvalidException("Restaurant name can only have alphanumeric characters, white space, !, ?, and '.");
-            }
-            else
-            {
-                this._name = value;
-            }
-        } 
-    }
+    [Required]
+    [RegularExpression("^[a-zA-Z0-9 !?']+$", ErrorMessage = "Restaurant name can only have alphanumeric characters, white space, !, ?, and '.")]
+    public string Name { get; set; }
+
+    //private string _name;
+    //public string Name {
+    //    get => _name;
+    //    set
+    //    {
+    //        Regex pattern = new Regex("^[a-zA-Z0-9 !?']+$");
+    //        if(string.IsNullOrWhiteSpace(value))
+    //        {
+    //            //we're checking whether this string is null or whitespace
+    //            throw new InputInvalidException("Name can't be empty");
+    //        }
+    //        //even if the string is not null or white space,
+    //        //we can still check for the name's validity by using RegEx (Regular Expression)
+    //        //Regular Expression is a way to pattern match a string for a certain condition
+    //        //it has a confusing syntax, so I recommend looking up language specific RegEx reference page to build one
+    //        else if(!pattern.IsMatch(value))
+    //        {
+    //            throw new InputInvalidException("Restaurant name can only have alphanumeric characters, white space, !, ?, and '.");
+    //        }
+    //        else
+    //        {
+    //            this._name = value;
+    //        }
+    //    } 
+    //}
 
 
     // //our own custom getter and setter for the private backing field
---
+++ 05SOA/RestaurantReviews/WebAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using BL;
+using CustomExceptions;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private IBL _bl;
+        public RestaurantController(IBL bl)
+        {
+            _bl = bl;
+        }
+        // GET: api/<RestaurantController>
+        [HttpGet]
+        public List<Restaurant> Get()
+        {
+            return _bl.GetAllRestaurants();
+        }
+
+        // GET api/<RestaurantController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RestaurantController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Restaurant restoToAdd)
+        {
+            try
+            {
+                _bl.AddRestaurant(restoToAdd);
+                return Created("Successfully added", restoToAdd);
+            }
+            catch (DuplicateRecordException ex)
+            {
+                return Conflict(ex.Message);
+            }
+        }
+
+        // PUT api/<RestaurantController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RestaurantController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
