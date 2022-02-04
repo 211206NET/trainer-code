3db8c888e0da74384257f7cea55399e50c1f0c1c
Juniper Song <songminseonk@gmail.com>
2022-02-04 15:44:51 -0500

    rest of testing

--- 09JS_TS_Angular/rrAng/src/app/restaurant-list/restaurant-list.component.spec.ts
+++ 09JS_TS_Angular/rrAng/src/app/restaurant-list/restaurant-list.component.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RestaurantListComponent } from './restaurant-list.component';
+import { RRApiService } from '../services/rrapi.service';
+import { Restaurant } from '../models/restaurant';
 
 describe('RestaurantListComponent', () => {
   let component: RestaurantListComponent;
   let fixture: ComponentFixture<RestaurantListComponent>;
-
+  let service: RRApiService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ RestaurantListComponent ],
       imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
+
+    service = TestBed.inject(RRApiService)
   });
 
   beforeEach(() => {
@@ -20,7 +24,61 @@ describe('RestaurantListComponent', () => {
     fixture.detectChanges();
   });
 
+  let fakeData: Restaurant[] = [
+    {
+      id: 1,
+      name: 'Salt and Straw',
+      streetAddress: '123 test ave',
+      city: 'test',
+      state: 'TS',
+      reviews: []
+    },
+    {
+      id: 2,
+      name: 'Another Test Restaurant',
+      streetAddress: '123 test ave',
+      city: 'test',
+      state: 'TS',
+      reviews: []
+    },
+  ];
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call the rrApiService', async () => {
+    spyOn(service, 'getAllRestaurant').and.returnValue(Promise.resolve(fakeData));
+
+    fixture = TestBed.createComponent(RestaurantListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    
+    await fixture.whenStable().then(() => 
+    {
+      expect(service.getAllRestaurant).toHaveBeenCalled();
+      expect(service.getAllRestaurant).toHaveBeenCalledTimes(1);
+      expect(component.allResto.length).toEqual(2);
+    })
+  })
+
+  it('should have restaurant table', async () => {
+    spyOn(service, 'getAllRestaurant').and.returnValue(Promise.resolve(fakeData));
+
+    fixture = TestBed.createComponent(RestaurantListComponent);
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    await fixture.whenStable().then(() => 
+    {
+      fixture.detectChanges();
+      expect(compiled.querySelector('#restaurant-table')).toBeTruthy();
+    })
+  });
+
+  it('should display no resto tag if no resto', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('#no-resto')).toBeTruthy();
+  })
 });
--- 09JS_TS_Angular/rrAng/src/app/services/rrapi.service.spec.ts
+++ 09JS_TS_Angular/rrAng/src/app/services/rrapi.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RRApiService } from './rrapi.service';
+import { Restaurant } from '../models/restaurant';
 
 describe('RRApiService', () => {
   let service: RRApiService;
@@ -15,4 +16,57 @@ describe('RRApiService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all restaurants', async () => {
+    let fakeData: Restaurant[] = [
+      {
+        id: 1,
+        name: 'Salt and Straw',
+        streetAddress: '123 test ave',
+        city: 'test',
+        state: 'TS',
+        reviews: []
+      },
+      {
+        id: 2,
+        name: 'Another Test Restaurant',
+        streetAddress: '123 test ave',
+        city: 'test',
+        state: 'TS',
+        reviews: []
+      },
+    ];
+    
+    spyOn(service, 'getAllRestaurant').and.returnValue(Promise.resolve(fakeData))
+
+    await service.getAllRestaurant().then((res) => {
+      //Verify that the response is what we want
+      expect(service.getAllRestaurant).toHaveBeenCalled();
+      expect(service.getAllRestaurant).toHaveBeenCalledTimes(1);
+      expect(res).toBeTruthy();
+      expect(res.length).toEqual(2);
+    });
+  });
+
+  it('should create restaurants', async () =>
+  {
+    let fakeInput: Restaurant = {
+      id: 1,
+      name: 'Salt and Straw',
+      streetAddress: '123 test ave',
+      city: 'test',
+      state: 'TS',
+      reviews: []
+    };
+
+    spyOn(service, 'createNewRestaurant').and.returnValue(Promise.resolve(fakeInput));
+
+    await service.createNewRestaurant(fakeInput).then((res) => {
+      expect(service.createNewRestaurant).toHaveBeenCalledTimes(1)
+      expect(service.createNewRestaurant).toHaveBeenCalledWith(fakeInput)
+
+      expect(res).toBeTruthy();
+      expect((res as Restaurant).id).toEqual(1);
+    })
+  })
 });
